99ca79fafffe10dbe9cb3ed48bd0662e3a110ab7
JaydenMikulcik <jjmikulcik@gmail.com>
2023-01-31 19:34:58 -0700

    About half done finished the left side styling

---
+++ topPortion.js
@@ -0,0 +1,19 @@
+document.write('<span class="spanTop">');
+document.write('    <a href="./about.html">About</a>');
+document.write("    <span> - </span>");
+document.write('    <a href="./skills.html">Skills</a>');
+document.write("    <span> - </span>");
+document.write('    <a href="./education.html">Education</a>');
+document.write("    <span> - </span>");
+document.write('    <a href="./experience.html">Exprience</a>');
+document.write("    <span> - </span>");
+document.write('    <a href="./interests.html">Interests</a>');
+document.write("    <span> - </span>");
+document.write('    <a href="./contact.html">Contact</a>');
+document.write("</span>");
+document.write("</br>");
+document.write(
+  '<img src="https://www.salesforce.com/content/dam/blogs/legacy/2014/05/6a00e54ee3905b883301a73dc2389e970.jpg" />'
+);
+document.write("");
+document.write("<h2>Business Person</h2>");
